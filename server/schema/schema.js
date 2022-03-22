4a2670c5f12202c4697aa1de5b1e48629bb2dd30
Sedat Taskiran <se_tas@yahoo.com>
2022-03-23 00:52:07 +0300

    Schema modified

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,11 +1,55 @@
 const graphql = require("graphql");
 
-const {GraphQLObjectType, GraphQLString} = graphql;
+const {GraphQLObjectType, GraphQLString, GraphQLID, GraphQLSchema} = graphql;
 
-const Author = new GraphQLObjectType({
+const PostType = new GraphQLObjectType({
+    name:"Post",
+    fields:()=> ({
+        id: {type: GraphQLID},
+        title: {type: GraphQLString},
+        category: {type: GraphQLString},
+        author: {
+            type: AuthorType,
+            resolve(parent,args){
+                
+            }
+        }
+    })
+});
+
+const AuthorType = new GraphQLObjectType({
     name:"Author",
-    fields:()=>
-})
+    fields:()=> ({
+        id: {type: GraphQLID},
+        name: {type: GraphQLString},
+        bio: {type: GraphQLString},
+        photo: {type: GraphQLString},
+    })
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: "RootQueryType",
+    fields:{
+        post:{
+            type:PostType,
+            args:{id:{type: GraphQLID}},
+            resolve(parent, args){
+                //get data from db/other source
+            }
+        },
+        author:{
+            type:AuthorType,
+            args:{id:{type: GraphQLID}},
+            resolve(parent, args){
+                //get data from db/other source
+            }
+        }        
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+});
 
 
 // query MyQuery {
