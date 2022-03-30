06c1257daa6dc671f3d3f0f245a158f866554320
Sedat Taskiran <se_tas@yahoo.com>
2022-03-30 23:14:33 +0300

    Connected to DB

--- server/app.js
+++ server/app.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const { graphqlHTTP } = require('express-graphql');
 const schema = require("./schema/schema");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv")
 
 const app = express();
+
+dotenv.config()
+
+mongoose.connect(process.env.CONNECTION_URL)
+mongoose.connection.once("open", () => {
+    console.log("Connected to the DB");
+})
+
 
 app.use("/graphql",graphqlHTTP({
     schema,
